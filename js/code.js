87526af89b555ff26bc35f852e8d656829a47f2c
Rafael <rafaelferreiracosta26@gmail.com>
2019-05-27 17:45:38 -0300

    Adding more variables to front, prettifying results and showing

--- js/code.js
+++ js/code.js
@@ -1,4 +1,6 @@
 function run() {
+    toggleBtns();
+
     let arrivedCars = 0;
     let endTimeInHours = 8;
     let arrivalTimes = 0;
@@ -7,12 +9,13 @@ function run() {
 
     endTimeInHours = parseInt(document.getElementById("duration").value)
     workersAmount = parseInt(document.getElementById("workersQt").value)
+    queueLimit = parseInt(document.getElementById("queueLimit").value)
     verbose = document.getElementById("verbose").checked
     infiniteRun = document.getElementById("infinite").checked
 
 
     let washer = new Washer();
-    // washer.queue.setLimit(-1);
+    washer.queue.setLimit(queueLimit);
 
     function arriveCars() {
         let time = exponential(2, 15, 1);
@@ -42,48 +45,90 @@ function run() {
 
         if (!infiniteRun) {
             if (arrivalTimes > endTimeInHours * 60) {
+                let res = {
+                    "Total Simulation time": arrivalTimes.toFixed(3) + ' mins =  (' + (arrivalTimes / 60).toFixed(3) + ') hrs',
+                    "ArrivedCars": arrivedCars,
+                    "Washed": washer.washed,
+                    "Washing": washer.washing,
+                    "In Queue": washer.queue.size(),
+                    "Max In Queue": maxInQueue,
+                    "Max Entities in System": maxEntities
+                }
+
                 clearTimeout(tm);
                 clearTimeout(washer.washInterval);
-                console.log('Total Simulation time: ', arrivalTimes.toFixed(3), 'mins =  (', (arrivalTimes / 60).toFixed(3), ') hrs')
-                console.log('ArrivedCars: ', arrivedCars)
-                console.log('Washed: ', washer.washed)
-                console.log('Washing: ', washer.washing)
-                console.log('In Queue: ', washer.queue.size())
-                console.log('Max In Queue: ', maxInQueue)
-                console.log('Max Entities in System: ', maxEntities)
+
+                Object.getOwnPropertyNames(res).forEach(function (val, idx, array) {
+                    console.log(val + ' -> ' + res[val]);
+                });
+
                 let i = 0;
                 washer.workers.forEach(worker => {
-                    console.log(`Worker[${i}] busy time: `, parseFloat(worker.workTime).toFixed(3), 'mins = (', ((worker.workTime / arrivalTimes) * 100).toFixed(3), '%)')
+                    console.log(`Worker[${i}] busy time: `, parseFloat(worker.workTime).toFixed(3), ' mins = (', ((worker.workTime / arrivalTimes) * 100).toFixed(3), '%)')
                     i++;
                 });
 
-                let results = []
-                results.push(arrivalTimes.toFixed(3))
-                results.push(arrivedCars)
-                results.push(washer.washed)
-                results.push(washer.washing)
-                results.push(washer.queue.size())
-                results.push(maxInQueue)
-                results.push(maxEntities)
+                let content = `<h5>Simulation [${simulations}]</h5> <div class="row">
+                <div class="col-md-6">
+                    <table class="tg">
+                        <tr>
+                            <th class="tg-7btt">Detail</th>
+                            <th class="tg-amwm">Result</th>
+                        </tr>`
 
-                results.forEach(result => {
-                    $("#results").append(`<div class='row'> <div class='col-md-6'> ${result}</div></div>`)
+                Object.getOwnPropertyNames(res).forEach(function (val, idx, array) {
+                    content += `<tr>
+                            <td class="tg-0lax">${val}</td>
+                            <td class="tg-0lax">${res[val]}</td>
+                        </tr>`
                 });
-                
+                content += `</table></div><div class="col-md-6">
+                <table class="tg">
+                    <tr>
+                        <th class="tg-7btt">Worker ID</th>
+                        <th class="tg-amwm">Ocupation</th>
+                    </tr>`;
 
+                let j = 0;
+                washer.workers.forEach(worker => {
+                    content += `<tr>
+                            <td class="tg-0lax">Worker[${j}]</td>
+                            <td class="tg-0lax">${parseFloat(worker.workTime).toFixed(3) + ' mins = (' + ((worker.workTime / arrivalTimes) * 100).toFixed(3) + '%)'}</td>
+                        </tr>`
+                    j++;
+                });
+                content += `</table></div></div><hr class='mb-4'>`
+                $("#results").append(content);
+
+                $('#resultArea').show();
+                toggleBtns();
             }
         }
     }
 
-
-
-
     arriveCars();
     washer.wash();
+    simulations++;
 }
+let simulations = 0;
 
 function stop() {
     infiniteRun = false
+}
+
+function toggleBtns() {
+    $('#runBtn').prop('disabled', function (i, v) {
+        return !v;
+    });
+    if (document.getElementById("infinite").checked) {
+        $('#stopBtn').prop('disabled', function (i, v) {
+            return !v;
+        });
+    }
+}
+
+function clearR() {
+    $("#results").empty();
 }
 
 document.getElementById("runBtn").addEventListener("click", function (event) {
@@ -97,6 +142,26 @@ document.getElementById("stopBtn").addEventListener("click", function (event) {
 document.querySelector('input[type="range"]').addEventListener('change', function (e) {
     speed = parseInt(this.value);
 });
+
+$("#aDist").change(function(){
+    $(this).find("option:selected").each(function(){
+        var optionValue = $(this).attr("value");
+        if(optionValue){
+            $(".arrbox").not("." + optionValue).hide();
+            $("." + optionValue + ".arrbox").show();
+        }
+    });
+}).change();
+
+$("#sDist").change(function(){
+    $(this).find("option:selected").each(function(){
+        var optionValue = $(this).attr("value");
+        if(optionValue){
+            $(".servbox").not("." + optionValue).hide();
+            $("." + optionValue + ".servbox").show();
+        }
+    });
+}).change();
 
 // $(document).ready(function () {
 //     console.log(document.getElementById("duration").value)
